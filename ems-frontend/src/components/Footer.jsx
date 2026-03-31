1ebe399da4575fc7a3b93442fa1822b9f03142c0
MONSIEUR-RBG <eyramrizbangou@gmail.com>
2026-03-31 17:47:49 +0200

    .....

--- ems-frontend/src/components/Footer.jsx
+++ ems-frontend/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
+  const date = new Date().getFullYear;
   return (
     <div>
         <footer className='footer'>
-            <span>&copy; Copiright All rights reserved , Eyram</span>
+            <span>&copy; Copiright All rights reserved , 2026</span>
             </footer>
             
             </div>
