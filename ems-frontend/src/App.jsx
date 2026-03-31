7288c642c888abd5f77c264585593119bd3f91e2
MONSIEUR-RBG <eyramrizbangou@gmail.com>
2026-03-31 17:38:37 +0200

    Officiellemenent Terminé en ce Jour , tout fonctionne correctement . Je suis  assez fier de moi . creation des Features Update et Delete avec leurs buttons et la logique Verificaiton des données du formulaire d'ajout et de modification avant validation Test reussis avec success

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,28 +1,35 @@
-import ListEmployee from './components/ListEmployee'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import EmployeeComponent from './components/EmployeeComponent'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ListEmployee from "./components/ListEmployee";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import EmployeeComponent from "./components/EmployeeComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
+   
+   <div className="vh-100">
+      <BrowserRouter >
         <Header />
         <Routes>
-          <Route path='/' element={<ListEmployee />} >
-
+          <Route path="/" element={<ListEmployee />}></Route>
+          <Route path="/employees" element={<ListEmployee />}>
+            {" "}
           </Route>
-          <Route path='/employees' element={<ListEmployee />} >  </Route>
-          <Route path='/add-employee' element={<EmployeeComponent />} >  </Route>
-
-
+          <Route path="/add-employee" element={<EmployeeComponent />}>
+            {" "}
+          </Route>
+          <Route
+            path="/edit-employee/:id"
+            element={<EmployeeComponent />}
+          >
+            {" "}
+          </Route>
         </Routes>
         <Footer />
       </BrowserRouter>
-    </>
-  )
+      </div>
+    
+  );
 }
 
-export default App
+export default App;
